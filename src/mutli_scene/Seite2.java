ad0b5ed88da78c297bb9a703c31336bfa6579eff
limmoor <luimm@gmx.de>
2018-02-22 10:01:38 +0100

    multi pages mit setCenter

---
+++ src/mutli_scene/Seite2.java
@@ -0,0 +1,10 @@
+package mutli_scene;
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+
+public class Seite2 extends HBox {
+    public Seite2() {
+        this.getChildren().add(new Button("Moin"));
+    }
+}
